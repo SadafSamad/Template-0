e2d285352e6b6db3f1764fcd614c09e40710063d
SadafSamad <asamad.as54@gmail.com>
2024-12-16 06:21:34 -0800

    adding the src directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,8 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
